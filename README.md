fa5d487477d99c317e4bf1fff5d8234e7fef374b
Linh Phan <linh.phan@partner.commerzbank.com>
2023-09-02 18:05:33 +0200

    Add Bootstrap

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# MeetingCreator
+# MeetingAssistant
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.0.
 
 ## Development server
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() =>
-    TestBed.configureTestingModule({
-      declarations: [AppComponent],
-    }),
-  );
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,18 +12,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MeetingCreator'`, () => {
+  it(`should have as title 'MeetingAssistant'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MeetingCreator');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'MeetingCreator app is running!',
-    );
+    expect(app.title).toEqual('MeetingAssistant');
   });
 });
